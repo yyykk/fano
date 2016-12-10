8aa4edf5de31222a19f25028b9a3233068b1b5e0
yyykk <yukaieecsu@gmail.com>
2016-12-10 23:25:03 +0800

    compress test

--- fano_recursive/fano.cpp
+++ fano_recursive/fano.cpp
@@ -17,7 +17,6 @@ public:
 	fano(string file_name);
 	void show();
 	string to_fano_code(string str = "");
-
 private:
 	int get_probability(string file_name);
 	int make_code(map_pointer begin_p, map_pointer end_p);
@@ -95,8 +94,8 @@ int fano::make_code(map_pointer high_pointer, map_pointer low_pointer){
 }
 
 void fano::show(){
-	for (auto &w : already){
-		cout << w.first << "\t" << w.second << endl;
+	for (auto &w : word){
+		cout << w.first << "\t" << w.second.first << "\t" << w.second.second << endl;
 	}
 }
 
@@ -113,10 +112,48 @@ string fano::to_fano_code(string str){
 	return code;
 }
 
+unsigned char bin_to_8(string::iterator bin){
+	unsigned char num8 = 0;
+	for (int i = 8; i != 0; --i, ++bin){
+		if (*bin == '1'){
+			num8 += pow(2, i - 1);
+		}
+	}
+	return num8;
+}
+
+void init8(string &str){
+	for (int i = (8 - (str.size() % 8)); i != 0; --i){
+		str += "0";
+	}
+}
+
+void test(char *file_name, string code){
+	//char md[100] = "md ";
+	//strcat(md, file_name);
+	//system(md);
+	ofstream outfile;
+	string file = "./test/" + string(file_name) + ".dat";
+	outfile.open(file, ios::binary | ios::out);
+	init8(code);
+	for (auto iter = code.begin(); iter != code.end(); iter += 8){
+		unsigned  char temp = bin_to_8(iter);
+		outfile.write((char*)&temp, sizeof(temp));
+	}
+	outfile.close();
+}
+
+
 int main(){
-	fano f("demo.txt");
-	f.show();
-	cout << f.to_fano_code() << endl;
+	char* text[5] = { "demo0", "demo1", "demo2", "demo3", "demo4" };
+	for (int i = 0; i < 5; ++i){
+		string file_name = "./test/" + string(text[i]) + ".txt";
+		fano *demo = new fano(file_name);//创建对象
+		string word_2_code = demo->to_fano_code();
+		demo->show();
+		test(text[i], word_2_code);
+		delete demo;
+	}
 	system("pause");
 }
 
